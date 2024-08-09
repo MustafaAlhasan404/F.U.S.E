4179e7a1b4d34a93a8d77332c5795a634924eb4c
Mustafa alhasan <mustafa.alhasan.2020@gmail.com>
2024-08-09 16:41:27 +0300

    updates

--- FUSE-EXPO/baseUrl.ts
+++ FUSE-EXPO/baseUrl.ts
@@ -1,4 +1,4 @@
-const baseUrl = "https://fuse-backend-x7mr.onrender.com" // Render Hosting
+const baseUrl = "https://3030-quiet-sunset-73461303.in-ws1.runcode.io"
 // const baseUrl = "http://192.168.43.239:3030"; // Nour's internet
 // const baseUrl = "http://192.168.1.103:3030"; // Nour's Home internet
 // const baseUrl = "http://192.168.103.132:3030"; // Dana's Internet
--- Fuse-VENDOR/baseUrl.ts
+++ Fuse-VENDOR/baseUrl.ts
@@ -1,2 +1,2 @@
-const baseUrl = "https://fuse-backend-x7mr.onrender.com"
+const baseUrl = "https://3030-quiet-sunset-73461303.in-ws1.runcode.io"
 export default baseUrl;
